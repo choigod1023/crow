37c8ae6fc05a0bdd98f2d90681a6d0e6f0375a18
jangjunhyeok <choigod10234@gmail.com>
2018-12-19 00:23:47 +0900

    mongoose

--- crow.js
+++ crow.js
@@ -2,11 +2,19 @@ const express = require('express')
 const app = express();
 const request = require('request');
 const cheerio = require('cheerio');
+var mongoose = require('mongoose');
+var autoIncrement = require('mongoose-auto-increment');
+var db = mongoose.connection;
+
 
 app.get('/api/:name', function (req, res) {
     let name = req.params.name;
     let url = `https://onoffmix.com/event/main?s=${name}`
     let base = "https://onoffmix.com"
+    let hash1, hash2, hash3, hash4, hash5
+    let hash = {
+        info: []
+    }
     let result = {
         info: []
     }
@@ -28,7 +36,8 @@ app.get('/api/:name', function (req, res) {
 
     setTimeout(function () {
         for (var j = 0; j < ddd.length; j++) {
-            let hash = []
+
+            let datetmp;
             request(ddd[j], function (err, response, body) {
                 const $ = cheerio.load(body);
                 let titleArr = $(`#content > div.content_wrapping.max_width_area > section.event_summary > div.right_area > h3`)
@@ -36,27 +45,44 @@ app.get('/api/:name', function (req, res) {
                 let image = $('#content > div.content_wrapping.max_width_area > section.event_summary > div.left_area > div.event_thumbnail.main_thumbnail > img')
                 let date = $('#content > div.content_wrapping.max_width_area > section.event_summary > div.right_area > ul > li:nth-child(1) > p')
                 let iframe = $('#eventDetail > iframe')
-                let hashtag1 = $('#content > div.content_wrapping.max_width_area > section.event_summary > div.right_area > div.tags > a:nth-child(' + String(1)+ ')')
-                let hashtag2 = $('#content > div.content_wrapping.max_width_area > section.event_summary > div.right_area > div.tags > a:nth-child(' + String(2)+ ')')
-                let hashtag3 = $('#content > div.content_wrapping.max_width_area > section.event_summary > div.right_area > div.tags > a:nth-child(' + String(3)+ ')')
-                let hashtag4 = $('#content > div.content_wrapping.max_width_area > section.event_summary > div.right_area > div.tags > a:nth-child(' + String(4)+ ')')
-                let hashtag5 = $('#content > div.content_wrapping.max_width_area > section.event_summary > div.right_area > div.tags > a:nth-child(' + String(5)+ ')')
-                hash.push(hashtag1[0].children[0].data)
-                hash.push(hashtag2[0].children[0].data)
-                hash.push(hashtag3[0].children[0].data)
-                hash.push(hashtag4[0].children[0].data)
-                if(hashtag5[0]!== undefined)
-                    hash.push(hashtag5[0].children[0].data)
-
+                let hashtag1 = $('#content > div.content_wrapping.max_width_area > section.event_summary > div.right_area > div.tags > a:nth-child(' + String(1) + ')')
+                let hashtag2 = $('#content > div.content_wrapping.max_width_area > section.event_summary > div.right_area > div.tags > a:nth-child(' + String(2) + ')')
+                let hashtag3 = $('#content > div.content_wrapping.max_width_area > section.event_summary > div.right_area > div.tags > a:nth-child(' + String(3) + ')')
+                let hashtag4 = $('#content > div.content_wrapping.max_width_area > section.event_summary > div.right_area > div.tags > a:nth-child(' + String(4) + ')')
+                let hashtag5 = $('#content > div.content_wrapping.max_width_area > section.event_summary > div.right_area > div.tags > a:nth-child(' + String(5) + ')')
+                hash1 = hashtag1[0].children[0].data.split("#")[1]
+                hash2 = hashtag2[0].children[0].data.split("#")[1]
+                hash3 = hashtag3[0].children[0].data.split("#")[1]
+                hash4 = hashtag4[0].children[0].data.split("#")[1]
+                if (hashtag5[0] !== undefined)
+                    hash5 = hashtag5[0].children[0].data.split("#")[1]
+                else
+                    hash5 = undefined
+                hash.info.push({
+                    temp1: hash1,
+                    temp2: hash2,
+                    temp3: hash3,
+                    temp4: hash4,
+                    temp5: hash5
+                })
+                if (date[0].children[0].data.split("~ ")[1].indexOf(" (") != -1) {
+                    datetmp = date[0].children[0].data.split("~ ")[1].split(" (")[0].replace(".", "-").replace(".", "-")
+                }
+                else {
+                    datetmp = date[0].children[0].data.split(" ~")[0].split(" (")[0].replace(".", "-").replace(".", "-")
+                }
                 result.info.push({
                     name: titleArr[0].attribs.title,
                     img: image[0].attribs.src,
                     content: titleArr[0].attribs.title,
                     date: {
-                        startDate: date[0].children[0].data.split(" ~")[0],
-                        endDate: date[0].children[0].data.split("~ ")[1]
+                        startDate: date[0].children[0].data.split(" ~")[0].split(" (")[0].replace(".", "-").replace(".", "-"),
+                        endDate: datetmp
                     },
-                    tag: hash,
+                    tag: [{
+                        "color": "green",
+                        "text": "외부대회",
+                    }],
                     link: base + iframe[0].attribs.src.split("/content")[0],
                     isJoin: true,
                     isApplicable: true
@@ -71,6 +97,7 @@ app.get('/api/:name', function (req, res) {
                 // }
                 // result.limitdate.push(limit[0].children[0].data)
             })
+
         }
     }, 5000)
 
@@ -111,11 +138,57 @@ app.get('/api/:name', function (req, res) {
                 var date = new Date(y, m - 1, d);
                 tmp[i] = date.setDate(date.getDate() + 1);
             }
+
         }
+
+        for (var i = 0; hash.info[i] !== undefined; i++) {
+            let hashtagging = []
+            hashtagging.push(hash.info[i].temp1, hash.info[i].temp2, hash.info[i].temp3, hash.info[i].temp4, hash.info[i].temp5)
+            for (var j = 0; j < hashtagging.length; j++) {
+                if (hashtagging[j] === undefined)
+                    break
+                else if (hashtagging[j].indexOf("초등") != -1 || hashtagging[j].indexOf("중등") != -1 || hashtagging[j].indexOf("고등") != -1 || hashtagging[j].indexOf("대학") != -1) {
+                    result.info[i].tag.push({
+                        "color": "red",
+                        "text": hashtagging[j]
+                    })
+                }
+                else if (hashtagging[j].indexOf("해커톤") != -1 || hashtagging[j].indexOf("공모전") != -1 || hashtagging[j].indexOf("캠프") != -1) {
+                    result.info[i].tag.push({
+                        "color": "orange",
+                        "text": hashtagging[j]
+                    })
+                }
+
+                else {
+                    result.info[i].tag.push({
+                        "color": "grey",
+                        "text": hashtagging[j]
+                    })
+                }
+            }
+        }
+        const mongodb = require("mongodb");
+        const uri = "mongodb+srv://andy:8CcdLiVD9PlMAdfj@thisismongodb-lk0as.mongodb.net/dicon?retryWrites=true"
+        const client = new mongodb.MongoClient(uri, { useNewUrlParser: true });
+        client.connect(err => {
+            const collection = client.db("dicon").collection("contestdatas");
+            console.log(result.info)
+            //for (var i = 0; i < result.info.length; i++) {
+            //    collection.insertOne(result.info[i]).then()
+            //}
+            collection.insertMany(result.info,function(err,data){
+                console.log(err);
+            })
+            client.close();
+        });
+
+
         res.json(result)
+
     }, 12000)
 })
 
-app.listen(3000, function () {
+app.listen(3003, function () {
     console.log('ad')
 })
